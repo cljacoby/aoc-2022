7b7697750bf9f9b03b954d0a73483a3af579d266
cljacoby <cljacoby@gmail.com>
2022-12-06 23:13:58 -0500

    Add test cases for day 6 part 2

--- dec-6-2022/src/lib.rs
+++ dec-6-2022/src/lib.rs
@@ -5,8 +5,9 @@ use std::collections::HashMap;
 
 // Solution could be improved by chosing a different data structure
 // for `window` which could combine the functionality of the HashMap
-// and VecDeque. We basically need a way to count the number of characters,
-// but also push/pop on both sides of the window.
+// and VecDeque. We basically need a way to count the number of characters
+// as we go, instead of recreating at each iteration, but also push/pop on
+// both sides of the window.
 
 fn uniq(window: &VecDeque<char>, window_len: usize) -> bool {
     let mut counts: HashMap<char, usize> = HashMap::with_capacity(window_len);
@@ -15,22 +16,20 @@ fn uniq(window: &VecDeque<char>, window_len: usize) -> bool {
             .and_modify(|n| *n += 1)
             .or_insert(1);
     }
-    println!("counts = {:?}", counts);
 
-    counts.keys().len() == 4
+    counts.keys().len() == window_len
 }
 
-fn find_uniq_chars(input: &String, len: usize) -> Option<usize> {
-    let mut window: VecDeque<char> = VecDeque::with_capacity(len);
+fn find_uniq_chars(input: &String, window_len: usize) -> Option<usize> {
+    let mut window: VecDeque<char> = VecDeque::with_capacity(window_len);
     for (i, c) in input.chars().enumerate() {
-        if window.len() == len {
+        if window.len() == window_len {
             window.pop_front();
         }
         window.push_back(c);
-        if uniq(&window, len) {
+        if uniq(&window, window_len) {
             return Some(i + 1);
         }
-        println!("i={:?}, window={:?}", i, window);
     }
 
     return None;
@@ -38,20 +37,12 @@ fn find_uniq_chars(input: &String, len: usize) -> Option<usize> {
 
 #[cfg(test)]
 mod tests {
-    use crate::find_uniq_chars;
 
-    #[test]
-    fn test() {
-        let tests = vec![
-            (String::from("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5),
-            (String::from("nppdvjthqldpwncqszvftbrmjlhg"), 6),
-            (String::from("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10),
-            (String::from("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11),
-        ];
+    use crate::*;
 
+    fn test(window_len: usize, tests: Vec<(String, usize)>) {
         for (input, solution) in tests.into_iter() {
-            println!("input = {:?}", input);
-            let result = find_uniq_chars(&input, 4)
+            let result = find_uniq_chars(&input, window_len)
                 .expect("Input had uniq sequence, but got None result");
 
             println!("input={:?}, result={:?}, solution={:?}",
@@ -59,5 +50,34 @@ mod tests {
 
             assert_eq!(result, solution);
         }
+    }
+
+    #[test]
+    fn test_part_1() {
+        let cases = vec![
+            (String::from("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5),
+            (String::from("nppdvjthqldpwncqszvftbrmjlhg"), 6),
+            (String::from("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10),
+            (String::from("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11),
+            (std::fs::read_to_string("input.txt").unwrap(), 1175),
+        ];
+        
+        let window_len = 4;
+        test(window_len, cases);
+    }
+    
+    #[test]
+    fn test_part_2() {
+        let cases = vec![
+            (String::from("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19),
+            (String::from("bvwbjplbgvbhsrlpgdmjqwftvncz"), 23),
+            (String::from("nppdvjthqldpwncqszvftbrmjlhg"), 23),
+            (String::from("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29),
+            (String::from("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26),
+            (std::fs::read_to_string("input.txt").unwrap(), 3217),
+        ];
+        
+        let window_len = 14;
+        test(window_len, cases);
     }
 }
